30447164def55b850f6e94c67c9baca2157de06c
Jon Sime <jonsime@gmail.com>
2013-05-12 01:59:32 -0400

    basic POD, enough to get pod coverage to pass while not being completely worthless information

--- lib/Catalyst/Authentication/Store/DBIx/DataStore/User.pm
+++ lib/Catalyst/Authentication/Store/DBIx/DataStore/User.pm
@@ -5,6 +5,25 @@ use warnings;
 
 use base Catalyst::Authentication::User;
 
+=head1 NAME
+
+Catalyst::Authentication::Store::DBIx::DataStore::User
+
+=head1 DESCRIPTION
+
+This module provides the User class for the DBIx::DataStore based Catalyst authentication
+plugin. This module is not intended to be used directly by anything other than
+Catalyst::Authentication::Store::DBIx::DataStore;
+
+=head1 METHODS
+
+=head2 new
+
+User class constructor. Requires a reference to the calling authentication store object
+followed by a hash reference containing all the available (or visible) user fields.
+
+=cut
+
 sub new {
     my ($class, $store, $userdata) = @_;
 
@@ -20,6 +39,12 @@ sub new {
     return $self;
 }
 
+=head2 id
+
+Returns the User ID.
+
+=cut
+
 sub id {
     my ($self) = @_;
 
@@ -27,9 +52,24 @@ sub id {
     return;
 }
 
+=head2 supported_features
+
+Returns an array (or undef) indicating which Catalyst authentication, authorization,
+and session features are supported.
+
+=cut
+
 sub supported_features {
     return undef;
 }
+
+=head2 get
+
+Returns the value of a specific field from the User class. Accepts a single argument,
+which should be the name of the field whose value is desired. Any failure results in
+the return of nothing.
+
+=cut
 
 sub get {
     my ($self, $field) = @_;
@@ -43,6 +83,12 @@ sub get {
         if exists $self->{$field};
     return;
 }
+
+=head2 get_object
+
+Returns the User object. Intended for use by the superclass.
+
+=cut
 
 sub get_object {
     my ($self) = @_;
